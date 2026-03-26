c2ba4efe147125752040ad8fda8181dad483305c
YuShan <113306056@nccu.edu.tw>
2026-03-26 15:18:50 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex h-full">
-      <div className="bg-gray-200 w-[320px] h-full">
+      <div className="bg-gray-200 w-[320px] rounded-full">
+      <Image src="/dog01.jpg" alt="dog01" />
+
 
       <div>大頭照</div>
       <div>名字</div>
@@ -18,7 +20,7 @@ export default function Home() {
       <div>我的興趣</div>
       <div>設計專案</div>
       <div>程式專案</div>
-      
+
       </div>
       </div>
 
