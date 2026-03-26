8e9c1dc0f2e82ff91063b4ccc72d3b3140c31724
YuShan <113306056@nccu.edu.tw>
2026-03-26 15:40:55 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-full p-4">
-      <div className="bg-gray-200 w-[320px] rounded-full">
-      <Image src="/dog01.jpg" alt="dog01" width={80} height={80}/>
+      <div className="bg-gray-200 w-[320px] rounded-full p-4">
+        <Image src="/dog01.jpg" alt="dog01" width={80} height={80} />
 
+        <div>大頭照</div>
+        <div>名字</div>
+        <div>簡介</div>
 
-      <div>大頭照</div>
-      <div>名字</div>
-      <div>簡介</div>
+        <div className="flex gap-3 mt-2">
+          <div>社群連結</div>
+          <div>社群連結</div>
+          <div>社群連結</div>
+        </div>
 
-      <div>
-        <div>社群連結</div>
-        <div>社群連結</div>
-        <div>社群連結</div>
+        <div className="mt-4 flex flex-col gap-2">
+          <Link href="/about" className="bg-gray-200">
+            我的興趣
+          </Link>
+          <Link href="/hobby" className="bg-gray-200">
+            設計專案
+          </Link>
+          <Link href="/hobby" className="bg-gray-200">
+            程式專案
+          </Link>
+        </div>
       </div>
-
-      <div className="bg-gray-200">我的興趣</div>
-      <div className="bg-gray-200">設計專案</div>
-      <div className="bg-gray-200">程式專案</div>
-
-      </div>
-      </div>
-
-   
-   
-        
+    </div>
   );
 }
