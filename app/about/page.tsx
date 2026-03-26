9ed0269fa0a7a654c99c20a6c7da932d4c0e5559
YuShan <113306056@nccu.edu.tw>
2026-03-26 15:41:02 +0800

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">我的興趣</h1>
+      <p className="mt-2 text-gray-700">這裡可以放你的興趣內容。</p>
+      <Link href="/" className="mt-6 inline-block underline">
+        回到首頁
+      </Link>
+    </div>
+  );
+}
+
